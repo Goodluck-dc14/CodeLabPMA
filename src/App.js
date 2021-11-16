0910687ef42d09dbf832dae9a0fb83bc4c2de947
Goodluck-dc14 <84187549+Goodluck-dc14@users.noreply.github.com>
2021-11-16 03:59:26 +0100

    onboarding page 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Onboarding1 from "./Goodluck/Onboarding1";
+// import Onboarding1 from "./Goodluck/Onboarding1";
 // import Onboarding2 from "./Goodluck/Onboarding2";
+import Onboarding3 from "./Goodluck/Onboarding3";
 
 const App = () => {
   return (
     <div>
-      <Onboarding1 />
-      {/* <Onboarding2 /> */}
+      {/* <Onboarding1 />
+      <Onboarding2 /> */}
+      <Onboarding3 />
     </div>
   );
 };
